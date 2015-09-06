7cc6ebe7e703a9a51897f69a238e7b9ba8ac0940
Daniel-PC\Daniel <danielhuamani15@gmail.com>
2015-09-05 22:20:02 -0500

    primero pasos
    primero pasos en php

---
+++ variables.php
@@ -0,0 +1,21 @@
+<?php
+	$nombre_footer = "Daniel huamani";
+	$titulo = "Hello world php";
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+		<header>
+			<h2><?php echo $titulo ?></h2>
+		</header>
+		<footer>
+			copyright hecho por : <h3><?php echo $nombre_footer ?></h3>
+		</footer>
+</body>
+</html>
